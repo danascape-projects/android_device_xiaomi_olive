e87b3eb57bb3f8ebd0a47f1338abe8607189e27e
danascape <saalimquadri1@gmail.com>
2021-03-12 18:03:26 +0100

    olive: Set TARGET_KERNEL_VERSION

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -92,6 +92,7 @@ BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
+TARGET_KERNEL_VERSION := 4.9
 
 # DTBO partition definitions
 BOARD_PREBUILT_DTBOIMAGE := device/xiaomi/olive-kernel/dtbo.img
